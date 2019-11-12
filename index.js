ee5b8fd5456c74fb3f6f8fcc3978a4393f0c8209
pngwn <hello@pngwn.io>
2019-11-12 10:11:09 +0000

    Fix imports/exports.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export { network } from './network.js';
-export { memory } from './memory.js';
-export { savedata } from './savedata.js';
-export { cpu } from './cpu.js';
+export { network } from './src/network.js';
+export { memory } from './src/memory.js';
+export { savedata } from './src/savedata.js';
+export { cpu } from './src/cpu.js';
