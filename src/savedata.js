f79bbeb76c99a0c2a526bbb016819108b7b5a5ae
pngwn <hello@pngwn.io>
2019-11-12 10:38:59 +0000

    Add save data sensor.

---
+++ src/savedata.js
@@ -0,0 +1,16 @@
+const savedata = () => {
+	if (
+		navigator !== undefined &&
+		'connection' in navigator &&
+		'saveData' in navigator.connection
+	) {
+		return {
+			supported: true,
+			saveData: navigator.connection.saveData === true,
+		};
+	} else {
+		return { supported: false };
+	}
+};
+
+export { savedata };
